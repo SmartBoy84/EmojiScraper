1570af7fed2e6734628aed8558a309c5b322b2a9
SmartBoy84 <barfie@gabba.ga>
2022-04-18 09:31:18 +0800

    FINALLY!

--- createCartridges.sh
+++ createCartridges.sh
@@ -10,13 +10,14 @@ if [ -d "${D}" ]; then
 echo "Converting ${D}"
 
 # convert files
+cd ${D}
 rename 's/\.([^.]+)$/.\L$1/' * # rename all upper case extensions to lower case
 mogrify -background black -alpha remove -alpha off -format PNG *.png[0] # remove transparency
 mogrify -depth 32 -alpha set -format bmp *.PNG # make bmp
 rm *.png && rm *.PNG # remove redundant png files
 
 # make buffers
-cd ..
+cd ../..
 echo $(ls $DIR/${D} | grep -e .bmp -e .BMP | wc -l) emojis available
 
 ./emojifier te $CAR/${D} $DIR/${D} 3 $(ls $DIR/${D} | grep -e .bmp -e .BMP | wc -l)
