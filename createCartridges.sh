d716407c396d0a9c73d9cef16ec4c5139b53f481
SmartBoy84 <barfie@gabba.ga>
2022-04-18 08:52:39 +0800

    Fix script

--- createCartridges.sh
+++ createCartridges.sh
@@ -1,10 +1,19 @@
 DIR=emojis
-
+CAR=cartridges
+mkdir $CAR
 cd $DIR
 
 for D in *; do
 if [ -d "${D}" ]; then
 echo "Converting ${D}"
-mogrify -background black -format png ${D}/* && mogrify -depth 32 -alpha set -format bmp ${D}/* && rm ${D}/*.png
+#mogrify -background black -format png ${D}/* && mogrify -depth 32 -alpha set -format bmp ${D}/* && rm ${D}/*.png
+
+cd ..
+echo $(ls $DIR/${D} | grep -e .bmp -e .BMP | wc -l) emojis available
+
+./emojifier te $CAR/${D} $DIR/${D} 3 $(ls $DIR/${D} | grep -e .bmp -e .BMP | wc -l)
+if [ $? -eq 1 ]; then ./emojifier te $CAR/${D} $DIR/${D} 1 $(ls $DIR/${D} | grep -e .bmp -e .BMP | wc -l); fi
+
+cd $DIR
 fi
 done
